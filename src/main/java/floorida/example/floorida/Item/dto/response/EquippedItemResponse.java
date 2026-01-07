a2e884772741a8f9f0032a45fa24c62152afd49d
whatimeisitnow <jayjay.hi.77@gmail.com>
2026-01-07 18:01:28 +0900

    fix/Item

--- src/main/java/floorida/example/floorida/Item/dto/response/EquippedItemResponse.java
+++ src/main/java/floorida/example/floorida/Item/dto/response/EquippedItemResponse.java
@@ -1,5 +1,6 @@
 package floorida.example.floorida.Item.dto.response;
 
+import floorida.example.floorida.Item.entity.Item;
 import floorida.example.floorida.Item.entity.ItemType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,4 +13,22 @@ public class EquippedItemResponse {
     private String itemName;
     private ItemType itemType;
     private String imageUrl;
+
+    // ✅ 추가
+    private Integer offsetX;
+    private Integer offsetY;
+    private Integer width;
+    private Integer height;
+
+    public EquippedItemResponse(Item item) {
+        this.itemId = item.getItemId();
+        this.itemName = item.getName();
+        this.itemType = item.getType();
+        this.imageUrl = item.getImgUrl();
+
+        this.offsetX = item.getOffsetX();
+        this.offsetY = item.getOffsetY();
+        this.width = item.getWidth();
+        this.height = item.getHeight();
+    }
 }
